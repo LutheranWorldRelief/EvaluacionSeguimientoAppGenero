9ac18742ae002a811288e44861e735db51ee65cd
Rodolfo Carmona <vladcarmona6@gmail.com>
2021-03-01 01:19:32 -0600

    UPDATED|CREATE helper batch scripts to develop in windows UPDATED docker-compose.yml attributes for ruby servic (stdin_open, tty). this helps to debug in docker environment

--- db-drop.sh
+++ db-drop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-@docker exec -it genero_db_1 bash -c "psql -U $${POSTGRES_USER} -d $${POSTGRES_USER} -c 'DROP DATABASE IF EXISTS '$${DB_NAME}"
+@docker exec -it genero_db_1 bash -c "psql -U $${POSTGRES_USER} -d $${DB_NAME} -c 'DROP DATABASE IF EXISTS '$${DB_NAME}"
 
--- unicorn.sh
+++ unicorn.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-unicorn -c config/unicorn.rb -E production
+if [ "${RAILS_ENV}" = "development" ]
+then
+  echo "Starting rails"
+  rm -f tmp/pids/server.pid && bundle exec rails s -p 3000 -b '0.0.0.0'
+fi
+
+if [ "${RAILS_ENV}" = "production" ]
+then
+  echo "Starting unicorn"
+  unicorn -c config/unicorn.rb -E production
+fi
+
