41b18f0c68f0cd8fa1757aa824da929a917755ad
“Rodolfo <vladcarmona6@gmail.com>
2021-08-22 17:03:33 -0600

    Added scripts to backup and drop database

--- db-drop.bat
+++ db-drop.bat
@@ -1,1 +1,3 @@
-@docker exec -it genero_db_1 bash -c "psql -U ${POSTGRES_USER} -d ${DB_NAME} -c 'DROP DATABASE IF EXISTS '${DB_NAME}"
+#!/bin/bash
+
+docker-compose exec db bash -c "psql -U \${POSTGRES_USER} -d \${DB_NAME} -c 'DROP DATABASE IF EXISTS '\${DB_NAME}"
